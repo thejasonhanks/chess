789d7ff6a45e1e9177968c598517c4c7d761bd9a
thejasonhanks <thejasonhanks@gmail.com>
2026-03-02 20:47:41 -0700

    push

--- server/src/main/java/server/ServerMain.java
+++ server/src/main/java/server/ServerMain.java
@@ -1,13 +1,9 @@
 package server;
 
-import chess.ChessGame;
-import chess.ChessPiece;
-
 public class ServerMain {
     public static void main(String[] args) {
         Server server = new Server();
         server.run(8080);
-        //var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
         System.out.println("♕ 240 Chess Server: ");
     }
 }
