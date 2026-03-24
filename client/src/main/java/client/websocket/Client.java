e09e1c103719cb65a855d4cc6bb84044e56bfcc8
thejasonhanks <thejasonhanks@gmail.com>
2026-03-24 16:55:06 -0600

    setting up client and clientmain

---
+++ client/src/main/java/client/websocket/Client.java
@@ -0,0 +1,44 @@
+package client.websocket;
+
+import java.util.Scanner;
+
+import com.google.gson.Gson;
+import model.*;
+import client.websocket.NotificationHandler;
+import server.ServerFacade;
+import client.websocket.WebSocketFacade;
+
+import static client.EscapeSequences.*;
+
+public class Client implements NotificationHandler {
+    private String name = null;
+    private final ServerFacade server;
+    private final WebSocketFacade ws;
+    private State state = State.SIGNEDOUT;
+
+    public Client(String serverUrl) throws Exception {
+        server = new ServerFacade(serverUrl);
+        ws = new WebSocketFacade(serverUrl, this);
+    }
+
+    public void run() {
+        System.out.println(LOGO + "♕ Welcome to Chess. Sign in to start. ♕");
+        System.out.print(help());
+
+        Scanner scanner = new Scanner(System.in);
+        var result = "";
+        while(!result.equals("quit")) {
+            printPrompt();
+            String line = scanner.nextLine();
+
+            try {
+                result = eval(line);
+                System.out.print(BLUE + result);
+            } catch(Throwable e){
+                var msg = e.toString();
+                System.out.print(msg);
+            }
+        }
+        System.out.println();
+    }
+}
