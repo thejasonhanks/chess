6ba2704c7b82c1d9ef0ba6fc5a6124a00d760c44
thejasonhanks <thejasonhanks@gmail.com>
2026-03-25 08:42:18 -0600

    implement register, login, and createGame

--- client/src/main/java/client/websocket/Client.java
+++ client/src/main/java/client/websocket/Client.java
@@ -1,18 +1,30 @@
-package client.websocket;
+package client;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
+import com.google.gson.Gson;
+import model.*;
+import exception.ResponseException;
 import client.websocket.NotificationHandler;
-import client.ServerFacade;
+import server.ServerFacade;
 import client.websocket.WebSocketFacade;
+import webSocketMessages.Notification;
 
 import static client.EscapeSequences.*;
+import request.*;
+import result.*;
 
 public class Client implements NotificationHandler {
-    private String name = null;
     private final ServerFacade server;
+    private String username = null;
     private final WebSocketFacade ws;
-    private State state = State.SIGNEDOUT;
+    private State state = State.LOGGEDOUT;
+
+    public enum State {
+        LOGGEDOUT,
+        LOGGEDIN
+    }
 
     public Client(String serverUrl) throws Exception {
         server = new ServerFacade(serverUrl);
@@ -38,5 +50,150 @@ public class Client implements NotificationHandler {
             }
         }
         System.out.println();
+    }
+
+    public void notify(Notification notiication) {
+        System.out.println(RED + notification.message());
+        printPrompt();
+    }
+
+    private void printPrompt() {
+        System.out.print("\n" + RESET + ">>>" + GREEN);
+    }
+
+    public String eval(String input) {
+        try {
+            String[] tokens = input.toLowerCase().split(" ");
+            String cmd = (tokens.length > 0) ? tokens[0] : "help";
+            String[] params = Arrays.copyOfRange(tokens, 1, tokens.length);
+            return switch (cmd) {
+                case "register" -> register(params);
+                case "login" -> login(params);
+                case "create" -> createGame(params);
+                case "list" -> listGames();
+                case "join" -> joinGame(params);
+                case "logout" -> logout();
+                case "quit" -> "quit";
+                default -> help();
+            };
+        } catch (ResponseException ex) {
+            return ex.getMessage();
+        }
+    }
+    public String register(String... params) throws ResponseException {
+        if (params.length >=3) {
+            var result = server.register(
+                    new RegisterRequest(params[0], params[1], params[2])
+            );
+
+            authToken = result.authToken();
+            username = result.username();
+            state = State.LOGGEDIN;
+
+            return "Registered and logged in as " + username;
+        }
+
+        throw new ResponseException(ResponseException.Code.ClientError, "Expected: <username> <password> <email>");
+    }
+
+    public String login(String... params) throws ResponseException {
+        if (params.length >= 2) {
+            var result = server.login(new LoginRequest(params[0], params[1]));
+
+            authToken = result.authToken();
+            username = result.username;
+            state = State.LOGGEDIN;
+
+            return "Logged in as " + username;
+        }
+
+        throw new ResponseException(ResponseException.Code.ClientError, "Expected: <username> <password>");
+    }
+
+    public String createGame(String... params) throws ResponseException {
+        assertSignedIn();
+        if (params.length >= 1) {
+            var result = server.createGame(authToken, new CreateRequest(params[0]));
+
+            return "Game created with ID: " + result.gameID();
+        }
+        throw new ResponseException(ResponseException.Code.ClientError, "Expected: <gameName>");
+    }
+
+    public String listPets() throws ResponseException {
+        assertSignedIn();
+        PetList pets = server.listPets();
+        var result = new StringBuilder();
+        var gson = new Gson();
+        for (Pet pet : pets) {
+            result.append(gson.toJson(pet)).append('\n');
+        }
+        return result.toString();
+    }
+
+    public String adoptPet(String... params) throws ResponseException {
+        assertSignedIn();
+        if (params.length == 1) {
+            try {
+                int id = Integer.parseInt(params[0]);
+                Pet pet = getPet(id);
+                if (pet != null) {
+                    server.deletePet(id);
+                    return String.format("%s says %s", pet.name(), pet.sound());
+                }
+            } catch (NumberFormatException ignored) {
+            }
+        }
+        throw new ResponseException(ResponseException.Code.ClientError, "Expected: <pet id>");
+    }
+
+    public String adoptAllPets() throws ResponseException {
+        assertSignedIn();
+        var buffer = new StringBuilder();
+        for (Pet pet : server.listPets()) {
+            buffer.append(String.format("%s says %s%n", pet.name(), pet.sound()));
+        }
+
+        server.deleteAllPets();
+        return buffer.toString();
+    }
+
+    public String signOut() throws ResponseException {
+        assertSignedIn();
+        ws.leavePetShop(visitorName);
+        state = State.SIGNEDOUT;
+        return String.format("%s left the shop", visitorName);
+    }
+
+    private Pet getPet(int id) throws ResponseException {
+        for (Pet pet : server.listPets()) {
+            if (pet.id() == id) {
+                return pet;
+            }
+        }
+        return null;
+    }
+
+    public String help() {
+        if (state == State.SIGNEDOUT) {
+            return """
+                    - signIn <yourname>
+                    - quit
+                    """;
+        }
+        return """
+                - list
+                - adopt <pet id>
+                - rescue <name> <CAT|DOG|FROG|FISH>
+                - adoptAll
+                - signOut
+                - quit
+                """;
+    }
+
+    private void assertSignedIn() throws ResponseException {
+        if (state == State.SIGNEDOUT) {
+            throw new ResponseException(ResponseException.Code.ClientError, "You must sign in");
+        }
     }
 }
