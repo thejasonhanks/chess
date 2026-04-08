1c505a6ab4f621c135d30e5f3ba43cb006fbdcb1
thejasonhanks <thejasonhanks@gmail.com>
2026-04-08 15:55:19 -0600

    implement complete websocketfacade

--- client/src/main/java/client/websocket/WebSocketFacade.java
+++ client/src/main/java/client/websocket/WebSocketFacade.java
@@ -1,9 +1,10 @@
 package client.websocket;
 
+import chess.ChessMove;
 import com.google.gson.Gson;
 import exception.ResponseException;
-import websocket.commands.UserGameCommand.*;
-import websocket.messages.ServerMessage.*;
+import websocket.commands.*;
+import websocket.messages.*;
 
 import jakarta.websocket.*;
 
@@ -11,25 +12,18 @@ import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
 
+@ClientEndpoint
 public class WebSocketFacade extends Endpoint {
     private Session session;
-    private NotificationHandler notificationHandler;
+    private final NotificationHandler notificationHandler;
+    private final Gson gson = new Gson();
 
     public WebSocketFacade(String url, NotificationHandler notificationHandler) throws ResponseException{
+        this.notificationHandler = notificationHandler;
         try {
-            url = url.replace("http", "ws");
-            this.notificationHandler = notificationHandler;
-
+            url = url.replace("http", "ws") + "/ws";
             WebSocketContainer container = ContainerProvider.getWebSocketContainer();
-            this.session = container.connectToServer(this, socketURI);
-
-            this.session.addMessageHandler(new MessageHandler.Whole<String>() {
-                @Override
-                public void onMessage(String message) {
-                    Notification notification = new Gson().fromJson(message, Notification.class);
-                    notificationHandler.notify(notification);
-                }
-            });
+            container.connectToServer(this, new URI(url));
         } catch (DeploymentException | IOException | URISyntaxException ex) {
             throw new ResponseException(ResponseException.Code.ServerError, ex.getMessage());
         }
@@ -37,6 +31,49 @@ public class WebSocketFacade extends Endpoint {
 
     @Override
     public void onOpen(Session session, EndpointConfig endpointConfig) {
+        this.session = session;
+        this.session.addMessageHandler((MessageHandler.Whole<String>) this::handleMessage);
+    }
 
+    private void handleMessage(String message) {
+        try {
+            ServerMessage base = gson.fromJson(message, ServerMessage.class);
+
+            switch (base.getServerMessageType()) {
+                case LOAD_GAME -> notificationHandler.loadGame(gson.fromJson(message, LoadGameMessage.class));
+                case NOTIFICATION -> notificationHandler.notify(gson.fromJson(message, NotificationMessage.class));
+                case ERROR -> notificationHandler.error(gson.fromJson(message, ErrorMessage.class));
+            }
+        } catch (Exception exception) {
+            exception.printStackTrace();
+        }
+    }
+
+    public void sendConnect(String authToken, int gameID) throws IOException {
+        UserGameCommand command = new UserGameCommand(UserGameCommand.CommandType.CONNECT, authToken, gameID);
+        send(command);
+    }
+
+    public void sendMakeMove(String authToken, int gameID, ChessMove move) throws IOException {
+        MakeMoveCommand command = new MakeMoveCommand(authToken, gameID, move);
+        send(command);
+    }
+
+    public void sendLeave(String authToken, int gameID) throws IOException {
+        UserGameCommand command = new UserGameCommand(UserGameCommand.CommandType.LEAVE, authToken, gameID);
+        send(command);
+    }
+
+    public void sendResign(String authToken, int gameID) throws IOException {
+        UserGameCommand command = new UserGameCommand(UserGameCommand.CommandType.RESIGN, authToken, gameID);
+        send(command);
+    }
+
+    private void send(Object command) throws IOException {
+        if (session != null && session.isOpen()) {
+            session.getBasicRemote().sendText(gson.toJson(command));
+        } else {
+            throw new IOException("WebSocket is not open");
+        }
     }
 }
